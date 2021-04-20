37c6c3f7f3d05c69d79d55fe2c211a7202888eec
LuigiGF <luigi.gottardello.fonseca@gmail.com>
2021-04-20 16:49:49 -0300

    Finalizando ex 1

---
+++ judge-01.c
@@ -0,0 +1,34 @@
+#include <stdio.h>
+#include <math.h>
+
+int primeNumberCalculator(int entry)
+{
+  int i;
+  for (i = 2; i < entry / 2; i++)
+  {
+    //se a entrada por i for igual a zero significa não primo
+    if (entry % i == 0)
+    {
+      return 0;
+    }
+  }
+  return 1;
+}
+int main(void)
+{
+  int value, i, count;
+  scanf("%d", &count);
+  for (i = 0; i < count; i++)
+  {
+    scanf("%d", &value);
+    if (primeNumberCalculator(value))
+    {
+      printf("%d 1\n", value);
+    }
+    else
+    {
+      printf("%d 0\n", value);
+    }
+  }
+  return 0;
+}
