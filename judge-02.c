bd881c68ddea053887264c0a34cc59292cc5e891
LuigiGF <luigi.gottardello.fonseca@gmail.com>
2021-04-29 11:53:17 -0300

    Finalizando 10 no judge

--- judge-02.c
+++ judge-02.c
@@ -3,44 +3,48 @@
 //GLOBAL VARIABLES
 //v = quantity of tickets needed to earn a book
 //books = result of how much books
-int v = 0, books = 0, aux = 0;
-
-//recursion function to calculate books
-void bookCalculator(int tickets)
+int v = 0;
+int calculaVales(int tickets)
 {
-  int earnedBooks = tickets / v;
-  aux += tickets % v;
-  books += earnedBooks;
-  if (aux >= v)
-  {
-    books += aux / v;
-    aux /= v;
-  }
-  //verify to finish the recursive function
-  if (tickets < v)
-  {
-    return;
-  }
-  bookCalculator(earnedBooks);
-  return;
+  if (tickets >= v)
+    return 1 + calculaVales(tickets - v + 1);
+  else
+    return 0;
 }
+//tentativa abaixo estava dando 2 erros
+//recursion function to calculate books
+// void bookCalculator(int tickets)
+// {
+//   int earnedBooks = tickets / v;
+//   aux += tickets % v;
+//   books += earnedBooks;
+//   if (aux >= v)
+//   {
+//     books += aux / v;
+//     aux /= v;
+//   }
+//   //verify to finish the recursive function
+//   if (tickets < v)
+//   {
+//     return;
+//   }
+//   bookCalculator(earnedBooks);
+//   return;
+// }
 
 int main(void)
 {
-  int money, price, tickets, n, i;
+  int money, price, tickets, n, i, books;
   scanf("%d", &n);
   for (i = 0; i < n; i++)
   {
     scanf("%d %d %d", &money, &price, &v);
-    if (money > 0)
-    {
-      tickets = money / price;
-      books = tickets;
-      bookCalculator(tickets);
-    }
+    //if (money > 0)
+    //{
+    tickets = money / price;
+    books = tickets + calculaVales(tickets);
+    //}
     printf("%d\n", books);
-    books = 0;
-    aux = 0;
   }
   return 0;
 }
