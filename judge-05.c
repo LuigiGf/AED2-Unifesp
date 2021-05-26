ff015ad9dadc944028cfbb3768ae4decfcf5fd42
LuigiGF <luigi.gottardello.fonseca@gmail.com>
2021-05-26 17:10:21 -0300

    Finalizando o judge 05

--- judge-05.c
+++ judge-05.c
@@ -1,102 +1,74 @@
-
-// C program to Count
-// Inversions in an array
-// using Merge Sort
 #include <stdio.h>
-#include <stdlib.h>
-
-int _mergeSort(int arr[], int temp[],
-               int left, int right);
-int merge(int arr[], int temp[],
-          int left, int mid,
-          int right);
-
-/* This function sorts the input array and returns the
-   number of inversions in the array */
-int mergeSort(int arr[], int array_size)
+int k = 0;
+void swap(int *a, int *b)
 {
-  int *temp = (int *)malloc(sizeof(int) * array_size);
-  return _mergeSort(arr, temp, 0,
-                    array_size - 1);
+  int t = *a;
+  *a = *b;
+  *b = t;
 }
 
-/* An auxiliary recursive function
-   that sorts the input
-  array and returns the number
-  of inversions in the array.
-*/
-int _mergeSort(int arr[], int temp[], int left, int right)
+int partition(int arr[], int low, int high)
 {
-  int mid, inv_count = 0;
-  if (right > left)
+  int pivot = arr[high]; // pivot
+  int i = (low - 1);
+
+  int j;
+  for (j = low; j <= high - 1; j++)
   {
-    /* Divide the array into two parts and call
-       _mergeSortAndCountInv() for each of the parts */
-    mid = (right + left) / 2;
-
-    /* Inversion count will be the sum of inversions in
-        left-part, right-part and number of inversions in
-        merging */
-    inv_count += _mergeSort(arr, temp, left, mid);
-    inv_count += _mergeSort(arr, temp, mid + 1, right);
-
-    /*Merge the two parts*/
-    inv_count += merge(arr, temp, left, mid + 1, right);
-  }
-  return inv_count;
-}
-
-/* This funt merges two sorted
-   arrays and returns inversion
-   count in the arrays.*/
-int merge(int arr[], int temp[], int left, int mid,
-          int right)
-{
-  int i, j, k;
-  int inv_count = 0;
-
-  i = left; /* i is index for left subarray*/
-  j = mid;  /* j is index for right subarray*/
-  k = left; /* k is index for resultant merged subarray*/
-  while ((i <= mid - 1) && (j <= right))
-  {
-    if (arr[i] <= arr[j])
+    if (arr[j] < pivot)
     {
-      temp[k++] = arr[i++];
-    }
-    else
-    {
-      temp[k++] = arr[j++];
-
-      /*this is tricky -- see above
-             * explanation/diagram for merge()*/
-      inv_count = inv_count + (mid - i);
+      i++;
+      swap(&arr[i], &arr[j]);
     }
   }
-
-  /* Copy the remaining elements of left subarray
-   (if there are any) to temp*/
-  while (i <= mid - 1)
-    temp[k++] = arr[i++];
-
-  /* Copy the remaining elements of right subarray
-   (if there are any) to temp*/
-  while (j <= right)
-    temp[k++] = arr[j++];
-
-  /*Copy back the merged elements to original array*/
-  for (i = left; i <= right; i++)
-    arr[i] = temp[i];
-
-  return inv_count;
+  swap(&arr[i + 1], &arr[high]);
+  return (i + 1);
 }
 
-/* Driver code*/
-int main(int argv, char **args)
+void quickSort(int arr[], int low, int high)
 {
-  int arr[] = {1, 20, 6, 4, 5};
-  printf(" Number of inversions are %d \n",
-         mergeSort(arr, 5));
-  getchar();
+  if (low < high)
+  {
+    int pi = partition(arr, low, high);
+    if (pi > k - 1)
+    {
+      return quickSort(arr, low, pi - 1);
+    }
+    if (pi < k - 1)
+    {
+      return quickSort(arr, pi + 1, high);
+    }
+    if (pi == k - 1)
+    {
+      return;
+    }
+  }
+}
+
+void printarFunc(int arr[], int end)
+{
+  printf("%do menor elemento eh o %d\n", k, arr[k - 1]);
+  int i = 0;
+  for (i = 0; i < end; i++)
+  {
+    printf("%d ", arr[i]);
+  }
+}
+
+int main()
+{
+  int i = 0, n = 0;
+
+  scanf("%d %d", &k, &n);
+  int arr[n];
+
+  for (i = 0; i < n; i++)
+  {
+    scanf("%d", &arr[i]);
+  }
+
+  quickSort(arr, 0, n - 1);
+  printarFunc(arr, n);
+
   return 0;
 }
